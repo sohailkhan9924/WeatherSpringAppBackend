68a474e5386dfaf4a98565a9095ac740aa5a5143
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-05-20 00:31:50 +0530

    ...

--- src/main/java/com/nagarro/weatherapi/Controller/WeatherController.java
+++ src/main/java/com/nagarro/weatherapi/Controller/WeatherController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,7 +27,7 @@ public class WeatherController {
 		Weather weatherApp = WeatherService.getWeatherData(city);
 		return ResponseEntity.status(HttpStatus.OK).body(weatherApp);
 	}
-	@GetMapping(value="/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String test() {
 		return "Hello World";
 	}
