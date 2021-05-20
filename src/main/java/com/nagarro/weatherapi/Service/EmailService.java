7dc29d8c17d208344129a0b6ed3a605a3e7fa18a
sohailkhan9924 <84475177+sohailkhan9924@users.noreply.github.com>
2021-05-20 11:51:48 +0530

    Update EmailService.java

--- src/main/java/com/nagarro/weatherapi/Service/EmailService.java
+++ src/main/java/com/nagarro/weatherapi/Service/EmailService.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
 
 import com.nagarro.weatherapi.Model.Weather;
 import com.nagarro.weatherapi.Values.Values;
 
+@Service
 public class EmailService {
 	
 	@Autowired
